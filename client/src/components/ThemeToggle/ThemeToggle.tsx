1dea88f90ee7060c1f81815df96efa8479f4ac5c
William Duan <38791932+duanwilliam@users.noreply.github.com>
2021-12-01 00:49:01 -0800

    feat: some styling related features/fixes, lazy commit message oop
    - make shadow outlines accent color on focus
    - fix: make outline buttons filled when focused
    - fix: make toggle keyboard accessible
    -

--- client/src/components/ThemeToggle/ThemeToggle.tsx
+++ client/src/components/ThemeToggle/ThemeToggle.tsx
@@ -3,16 +3,25 @@ import { c } from '@/utils/cls';
 
 import styles from './.module.scss';
 
+import type { KeyboardEventHandler } from 'react';
+
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useThemeContext();
   const isLightMode = theme === 'light';
+
+  const onKeyDown: KeyboardEventHandler<HTMLDivElement> = (e) => {
+    if (!['Enter', ' ', 'Spacebar'].includes(e.key)) { return; }
+    toggleTheme();
+  }
 
   return (
     <div
       role="button"
       className={c(styles.toggle, isLightMode ? styles.light : styles.dark)}
       onClick={toggleTheme}
+      onKeyDown={onKeyDown}
       aria-label="Switch between light and dark mode"
+      tabIndex={0}
     >
       <span className={styles.icons} />
     </div>
