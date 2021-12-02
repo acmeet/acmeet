0bcc092cca2536d42dd339a36a885f407c26b3f4
William Duan <38791932+duanwilliam@users.noreply.github.com>
2021-12-01 17:24:00 -0800

    refactor(client): clean up responder label code

--- client/src/sections/meet/availabilities/hooks/useRespondersLabel.ts
+++ client/src/sections/meet/availabilities/hooks/useRespondersLabel.ts
@@ -28,24 +28,22 @@ export const useRespondersLabel = ({
   const label = useMemo(() => {
     switch (view) {
       case 'view': {
+        let grids: AvailabilityGrid[];
+        let suffix = '';
         if (selectedResponders.size > 0 && selectedResponders.size < availabilities.length) { // subset
-          if (hoveredSlot !== undefined) {
-            const count = countAvailability(selectedAvailabilityGrids, ...hoveredSlot);
-            return `Responders (${count}/${selectedResponders.size})`;
-          } else if (selectedSlot !== undefined) {
-            const count = countAvailability(selectedAvailabilityGrids, ...selectedSlot);
-            return `Responders (${count}/${selectedResponders.size})`;
-          }
-          return `Responders (${selectedResponders.size} selected)`;
-        } // all
-        if (hoveredSlot !== undefined) {
-          const count = countAvailability(availabilityGrids, ...hoveredSlot);
-          return `Responders (${count}/${availabilities.length})`;
-        } else if (selectedSlot !== undefined) {
-          const count = countAvailability(availabilityGrids, ...selectedSlot);
-          return `Responders (${count}/${availabilities.length})`;
+          grids = selectedAvailabilityGrids;
+          suffix = ' selected';
+        } else {
+          grids = availabilityGrids;
         }
-        return `Responders (${availabilities.length})`;
+        if (hoveredSlot !== undefined) {
+          const count = countAvailability(grids, ...hoveredSlot);
+          return `Responders (${count}/${grids.length}${suffix})`;
+        } else if (selectedSlot !== undefined) {
+          const count = countAvailability(grids, ...selectedSlot);
+          return `Responders (${count}/${grids.length}${suffix})`;
+        }
+        return `Responders (${grids.length}${suffix})`;
       }
       default: return `Responders (${availabilities.length})`;
     }
